5c8f641dd6d169ee71ddcd1a6c9f619506944c8f
Berran Remzi <11856339+BerranRemzi@users.noreply.github.com>
2026-06-17 00:39:10 +0300

    chore(config): update axis steps and enable babystepping features

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1351,7 +1351,7 @@
  * Override with M92 (when enabled below)
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 50, 50, 200, 50 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 100 }
 
 /**
  * Enable support for M92. Disable to save at least ~530 bytes of flash.
